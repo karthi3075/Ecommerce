094c36fc734231a4218665695402d0527e8eea30
karthi3075 <karthijayakumar3007@gmail.com>
2026-06-15 15:35:39 +0530

    unneccessary files is removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Generate from './Generate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    {/* <App /> */}
-    <Generate/>
+    <App />
   </>
 )
